3a4e1cbbcd0f2c200f20e38aaf987b433a695650
Thi Nguyen <anhthi.ieig@gmail.com>
2024-11-06 16:38:29 +0700

    Add react query dev tools

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toast } from "@usy-ui/base";
 import * as ReactDOM from "react-dom/client";
 
@@ -11,9 +12,10 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Toast />
     <QueryClientProvider client={queryClient}>
+      <Toast />
       <TodoList />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
